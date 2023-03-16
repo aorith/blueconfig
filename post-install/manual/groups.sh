974786483c2196f8b421dd29bf496dcfa0789b7d
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-03-16 19:49:57 +0100

    Update groups bootstrap script

--- post-install/manual/groups.sh
+++ post-install/manual/groups.sh
@@ -2,5 +2,12 @@
 
 if ! grep -q libvirt /etc/group; then
     grep -E '^libvirt:' /usr/lib/group | sudo tee -a /etc/group
+fi
+
+if ! grep -q docker < <(groups); then
+    sudo usermod -aG docker aorith
+fi
+
+if ! grep -q libvirt < <(groups); then
     sudo usermod -aG libvirt aorith
 fi
