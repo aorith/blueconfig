6bf8ddd100f07db547bd697aae4abe965c673d57
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-03-21 20:32:20 +0100

    feat: built in fonts

--- Containerfile
+++ Containerfile
@@ -5,10 +5,9 @@ FROM quay.io/fedora-ostree-desktops/silverblue:${FEDORA_MAJOR_VERSION} AS builde
 ARG VERSION
 RUN sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Fedora Linux ${VERSION} \(Blueconfig\)\"," /usr/lib/os-release
 
-ADD build.sh /tmp/build.sh
-COPY etc /etc
+COPY files/etc /etc
 
+ADD build.sh /tmp/build.sh
 RUN bash /tmp/build.sh
 
-RUN rm -rf /tmp/* /var/* && mkdir -p /var/tmp && chmod -R 1777 /var/tmp
 RUN rpm-ostree cleanup -m && ostree container commit
--- build.sh
+++ build.sh
@@ -13,6 +13,13 @@ rpm-ostree install /tmp/rpms/*.rpm fedora-repos-archive
 # Experimental: https://coreos.github.io/rpm-ostree/ex-rebuild/
 rpm-ostree ex rebuild
 
+# Fonts
+wget -P /tmp/fonts "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/NerdFontsSymbolsOnly.zip"
+find /tmp/fonts -type f -name "*Windows*" -delete
+find /tmp/fonts -type f -not -name "*Complete.ttf" -delete
+mkdir -p /usr/share/fonts/NerdFonts
+cp /tmp/fonts/*.ttf /usr/share/fonts/NerdFonts/
+
 # Enable automatic updates
 sed -i 's,.*AutomaticUpdatePolicy=.*,AutomaticUpdatePolicy=stage,g' /etc/rpm-ostreed.conf
 systemctl enable rpm-ostreed-automatic.timer
@@ -20,3 +27,9 @@ systemctl enable rpm-ostree-countme.timer
 
 # Disable suspend & hibernate
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# Cleanup
+rm -rf /tmp/*
+rm -rf /var/*
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
