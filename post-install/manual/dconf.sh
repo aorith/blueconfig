ad168ba9b10b6546da110673f97e41d92da7eb00
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-17 17:42:03 +0200

    small fixes

--- post-install/manual/dconf.sh
+++ post-install/manual/dconf.sh
@@ -6,7 +6,7 @@ dconf write /org/gnome/desktop/privacy/remember-recent-files false
 dconf write /org/gnome/desktop/privacy/remove-old-temp-files true
 dconf write /org/gnome/desktop/privacy/remove-old-trash-files true
 dconf write /org/gnome/desktop/privacy/old-files-age 'uint32 30'
-dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+#dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
 
 gsettings set org.gnome.desktop.peripherals.keyboard delay 300
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
