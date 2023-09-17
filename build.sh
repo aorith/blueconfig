d4d0d49da87a2503dfe2cc3174abeb7f8a35db84
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-17 11:49:51 +0200

    remove fonts from base

--- build.sh
+++ build.sh
@@ -13,14 +13,6 @@ rpm-ostree install /tmp/rpms/*.rpm fedora-repos-archive
 # Experimental: https://coreos.github.io/rpm-ostree/ex-rebuild/
 rpm-ostree ex rebuild
 
-# Fonts
-wget -P /tmp/fonts "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/NerdFontsSymbolsOnly.zip"
-( cd /tmp/fonts/ && unzip -o NerdFontsSymbolsOnly.zip; )
-find /tmp/fonts -type f -name "*Windows*" -delete
-find /tmp/fonts -type f -not -name "*Complete.ttf" -delete
-mkdir -p /usr/share/fonts/NerdFonts
-cp /tmp/fonts/*.ttf /usr/share/fonts/NerdFonts/
-
 # Enable automatic updates
 sed -i 's,.*AutomaticUpdatePolicy=.*,AutomaticUpdatePolicy=stage,g' /etc/rpm-ostreed.conf
 systemctl enable rpm-ostreed-automatic.timer
