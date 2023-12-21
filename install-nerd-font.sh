cd38acd2f279443a36b69e4095fa96467204088e
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-12-21 11:05:14 +0100

    fix

--- install-nerd-font.sh
+++ install-nerd-font.sh
@@ -12,3 +12,4 @@ tar --no-same-owner -xJf "/tmp/${FN}.tar.xz" -C "/usr/share/fonts/${FN}/"
 rm -f "/tmp/${FN}.tar.xz"
 rm -f "/usr/share/fonts/${FN}"/*Windows*
 chmod 0644 "/usr/share/fonts/${FN}"/*
+fc-cache -sf
